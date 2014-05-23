3b17ec8d45430642e80d65559090d0dbe7bdf951
SUIKKIS <katiskoski95@hotmail.com>
2014-05-23 09:42:55 +0300

    Updated movement keys, added lasers

--- awsumPeli-core/src/com/superProduction/awsum/awsumPeli.java
+++ awsumPeli-core/src/com/superProduction/awsum/awsumPeli.java
@@ -1,30 +1,45 @@
 package com.superProduction.awsum;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class awsumPeli extends ApplicationAdapter {
 
 	public static final String VERSION = "0.0.0.2 Pre-Alpha";
+
 	// Textures
 	Texture texture_player;
-	Texture fireBall;
+	Texture laser;
 	Texture backGround;
+
+	// Random lasers
+	Array<Rectangle> lasers;
+	long spawnTime;
+
 	// Sounds
 	Sound hitSound;
 	Music muzak;
+
 	// Camera
 	OrthographicCamera camera;
+
 	// 2D Render
 	SpriteBatch batch;
+
 	// Player logic
 	Rectangle player;
 
@@ -34,7 +49,7 @@ public class awsumPeli extends ApplicationAdapter {
 		// Load images textures for player, background and flying fireballs
 		backGround = new Texture("background.png");
 		texture_player = new Texture("player1.png");
-		fireBall = new Texture("laser.png");
+		laser = new Texture("laser.png");
 
 		// Load sound effect and background music
 		hitSound = Gdx.audio.newSound(Gdx.files.internal("hit.mp3"));
@@ -43,6 +58,7 @@ public class awsumPeli extends ApplicationAdapter {
 		// set music to loop and set it to play immediately
 		muzak.setLooping(true);
 		muzak.play();
+		muzak.setVolume((float) 0.2);
 
 		// Camera
 		camera = new OrthographicCamera();
@@ -58,6 +74,10 @@ public class awsumPeli extends ApplicationAdapter {
 		player.width = 64;
 		player.height = 64;
 
+		// Lasers
+		lasers = new Array<Rectangle>();
+		spawnLaser();
+
 	}
 
 	@Override
@@ -67,13 +87,39 @@ public class awsumPeli extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		// Updating camera position
-		camera.update();
 
+		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(texture_player, player.x, player.y);
+		
+		for(Rectangle laserReal : this.lasers) {
+			
+			batch.draw(laser, laserReal.x, laserReal.y);
+			
+		}
+		
 		batch.end();
+		
+		// Laser spawn and garbage collection
+		if (TimeUtils.nanoTime() - spawnTime > 100000000)
+			spawnLaser();
 
+		Iterator<Rectangle> iter = lasers.iterator();
+		while (iter.hasNext()) {
+
+			Rectangle laser = iter.next();
+			laser.y -= 200 * Gdx.graphics.getDeltaTime();
+			if (laser.y + 37 < 0)
+				iter.remove();
+			if(laser.overlaps(player)) {
+				
+				hitSound.play();
+				iter.remove();
+				
+			}
+		}
+		
 		// Touch
 
 		if (Gdx.input.isTouched()) {
@@ -83,8 +129,44 @@ public class awsumPeli extends ApplicationAdapter {
 			camera.unproject(osoitin);
 			player.x = osoitin.x - 64 / 2;
 			player.y = osoitin.y - 64 / 2;
-
 		}
 
+		// Checking if player uses keyboard
+
+		if (Gdx.input.isKeyPressed(Keys.LEFT))
+			player.x -= 200 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Keys.RIGHT))
+			player.x += 200 * Gdx.graphics.getDeltaTime();
+
+		if (Gdx.input.isKeyPressed(Keys.UP))
+			player.y += 200 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Keys.DOWN))
+			player.y -= 200 * Gdx.graphics.getDeltaTime();
+
+		// Checking boundaries
+
+		if (player.x < 0)
+			player.x = 0;
+		if (player.x > 736)
+			player.x = 736;
+
+		if (player.y < 0)
+			player.y = 0;
+		if (player.y > 336)
+			player.y = 336;
+
 	}
+
+	private void spawnLaser() {
+
+		Rectangle laser = new Rectangle();
+		laser.x = MathUtils.random(0, 800 - 37);
+		laser.y = 480;
+		laser.width = 13;
+		laser.height = 37;
+		this.lasers.add(laser);
+		spawnTime = TimeUtils.nanoTime();
+
+	}
+
 }
